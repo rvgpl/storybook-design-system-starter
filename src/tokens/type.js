7b1f33003fa0eeead62a2798b5c6b7f38f9e7015
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-05-21 21:16:59 +0530

    Setup design tokens

---
+++ src/tokens/type.js
@@ -0,0 +1,12 @@
+export const primaryFont = '"Roboto Mono", monospace';
+
+export const typeScale = {
+  jumbo: "1.8rem",
+  huge: "1.6rem",
+  xxl: "1.4rem",
+  xl: "1.2rem",
+  l: "1.1rem",
+  m: "1rem",
+  s: "0.8rem",
+  xs: "0.7rem",
+};
