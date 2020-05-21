b898fff33fb51a92b02b203052024024cf55f299
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-05-21 22:51:42 +0530

    Adds custom theme

---
+++ .storybook/customTheme.js
@@ -0,0 +1,37 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  colorPrimary: "rgb(71,31,8)",
+  colorSecondary: "orange",
+
+  brandTitle: "Custom Storybook",
+  brandUrl: "https://example.com",
+  brandImage:
+    "https://www.mangologisticsgroup.co.uk/couriers/wp-content/uploads/sites/3/2017/07/Apple-Logo-Transparent-Background-1024x768.png",
+});
+
+// // UI
+// appBg: "white",
+// appContentBg: "white",
+// appBorderColor: "silver",
+// appBorderRadius: 4,
+
+// // Typography
+// fontBase: '"Open Sans", sans-serif',
+// fontCode: "monospace",
+
+// // Text colors
+// textColor: "black",
+// textInverseColor: "rgba(255,255,255,0.9)",
+
+// // Toolbar default and active colors
+// barTextColor: "silver",
+// barSelectedColor: "white",
+// barBg: "black",
+
+// // Form colors
+// inputBg: "black",
+// inputBorder: "silver",
+// inputTextColor: "black",
+// inputBorderRadius: 4,
