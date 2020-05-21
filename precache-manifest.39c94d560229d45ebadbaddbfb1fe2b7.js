845240fadb21373f297649d72806b299a5b820ec
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-05-21 23:24:16 +0530

    Updates

---
+++ precache-manifest.39c94d560229d45ebadbaddbfb1fe2b7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a89528aac849b0dba2c711efe3da250",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.cc0fd12acb65706de9ad.bundle.js"
+  },
+  {
+    "url": "runtime~main.cc0fd12acb65706de9ad.bundle.js"
+  },
+  {
+    "url": "vendors~main.cc0fd12acb65706de9ad.bundle.js"
+  },
+  {
+    "url": "vendors~main.cc0fd12acb65706de9ad.bundle.js.LICENSE.txt"
+  }
+]);
