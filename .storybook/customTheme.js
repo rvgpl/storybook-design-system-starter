b9ae4cf4c52a9fe5450a1ab8224b9fa1b5634a03
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-05-21 22:57:25 +0530

    Update logo for storybook

--- .storybook/customTheme.js
+++ .storybook/customTheme.js
@@ -7,31 +7,5 @@ export default create({
 
   brandTitle: "Custom Storybook",
   brandUrl: "https://example.com",
-  brandImage:
-    "https://www.mangologisticsgroup.co.uk/couriers/wp-content/uploads/sites/3/2017/07/Apple-Logo-Transparent-Background-1024x768.png",
+  brandImage: "https://pngimg.com/uploads/apple_logo/apple_logo_PNG19678.png",
 });
-
-// // UI
-// appBg: "white",
-// appContentBg: "white",
-// appBorderColor: "silver",
-// appBorderRadius: 4,
-
-// // Typography
-// fontBase: '"Open Sans", sans-serif',
-// fontCode: "monospace",
-
-// // Text colors
-// textColor: "black",
-// textInverseColor: "rgba(255,255,255,0.9)",
-
-// // Toolbar default and active colors
-// barTextColor: "silver",
-// barSelectedColor: "white",
-// barBg: "black",
-
-// // Form colors
-// inputBg: "black",
-// inputBorder: "silver",
-// inputTextColor: "black",
-// inputBorderRadius: 4,
